204b463db85160712de872be0bde11ba0b66306f
Bakhtiyor Esanov <bahtiyoresanov@gmail.com>
2021-09-22 21:36:45 +0300

    GetById

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,18 +4,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
     [ApiController]
     [Route("")]
-    public class CelestialObjectController:ControllerBase
+    public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext _context)
         {
             this._context = _context;
+        }
+
+        [HttpGet("{id:int}",Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var matchedClSObject = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+            if (matchedClSObject == null)
+            {
+                return NotFound();
+            }
+            var takeSateliteProperty = matchedClSObject.Satellites.FirstOrDefault(c => c.OrbitedObjectId == id);
+            if (takeSateliteProperty != null)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var matchedClSObject = _context.CelestialObjects.FirstOrDefault(c => c.Name == name);
+            if (matchedClSObject == null)
+            {
+                return NotFound();
+            }
+            foreach (var it in matchedClSObject)
+            {
+
+            }
         }
     }
 }
